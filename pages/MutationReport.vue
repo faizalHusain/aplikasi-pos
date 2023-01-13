046921c5bb5016359f02c665d759275c1a372571
zidan utomo <zidanutomo@gmail.com>
2023-01-13 14:42:27 +0700

    RAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPIRAPI

--- pages/MutationReport.vue
+++ pages/MutationReport.vue
@@ -20,12 +20,45 @@
           ></v-text-field>
         </v-card-title>
     </div>
-    <v-data-table
-      :headers="headers"
-      :items="mutations_display"
-      :search="search"
-    >
-    </v-data-table>
+    <v-row >
+      <v-col
+        cols="4"
+        sm="6"
+        md="6"
+      >
+        <v-text-field
+          v-model="dateRangeText"
+          label="Date range"
+          prepend-icon="mdi-calendar"
+          readonly
+        ></v-text-field>
+      </v-col>
+      <v-col
+        cols="8"
+        sm="6"
+        md="6">
+        <v-data-table
+          :headers="headers"
+          :items="mutations_display"
+          :search="search"
+        >
+        </v-data-table>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col
+        cols="8"
+        sm="6"
+      >
+        <v-date-picker
+          v-model="dates"
+          range
+        ></v-date-picker>
+      </v-col>
+      <v-col>
+        <v-btn color="primary" class="mt-3" @click="submitDate">Submit</v-btn>
+      </v-col>
+    </v-row>
     <p class="mt-3">{{ message }}</p>
   </div>
 </template>
@@ -40,6 +73,9 @@ export default {
       message: "",
       title: "",
       search: "",
+      dates: [(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10), 
+      (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)],
+      
       headers: [
         { text: "Product Name", align: "start", sortable: true, value: "product_name", },
         { text: "Total Adjust", value: "total_adjust", sortable: true, },
@@ -47,11 +83,29 @@ export default {
         { text: "Total Sell", value: "total_sell", sortable: false, },
         { text: "Initial Stock", value: "initial_stock", sortable: true },
         { text: "Final Stock", value: "final_stock", sortable: false, },
-        { text: "Actions", value: "actions", sortable: false },
       ],
     };
   },
+  computed: {
+    dateRangeText () {
+      return this.dates.join(' ~ ')
+    },
+  },
   methods: {
+    submitDate(){
+      var data = {
+        start_date: this.dates[0],
+        end_date: this.dates[1],
+      };
+      this.$axios.$post(`mutationreport`, data)
+        .then((response) => {
+          this.mutations = response.data;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
     getDisplayMutation(mutation) {
       return {
         product_id: mutation.product_id,
